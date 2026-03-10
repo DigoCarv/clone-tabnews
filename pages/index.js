8a5c3bc97abaa039edc2dce86f6cc56ed9326ce0
Rodrigo Carvalhaes Gomes <158113800+DigoCarv@users.noreply.github.com>
2026-03-10 12:07:35 +0000

    mudanca de nome para dani

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Miguel, eu te amo, muito, muito, muito!
+    return <h1>Dani, eu te amo, muito, muito, muito!
   você é a melhor coisa que já aconteceu na minha vida, e eu sou muito grato por ter você ao meu lado. Eu te amo mais do que palavras podem expr minha melhor amiga, minha confidente, minha parceira de vida, e eu te amo mais do que tudo. Obrigado por ser quem você é, e por me amar do jeito que eu sou. Eu te amo para sempre!   
     </h1>
 }
